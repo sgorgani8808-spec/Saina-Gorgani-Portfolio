ce3ff986cfa1705abd44d6b9cbd9d679ac386234
sgorgani8808-spec <s.gorgani.8808@westcliff.edu>
2026-04-26 23:18:29 -0700

    fix app imports for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "./components/ui/toaster";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClientInstance } from "@/lib/query-client";
+import { queryClientInstance } from "./lib/query-client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import PageNotFound from "@/lib/PageNotFound";
-import Home from "@/pages/Home";
+import PageNotFound from "./lib/PageNotFound";
+import Home from "./pages/Home";
 
 function App() {
   return (
